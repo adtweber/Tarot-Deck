b8177b6ec90841a47b99c73e74d63d5381b67c7e
Andi Weber <adtweber@gmail.com>
2024-04-03 19:38:13 -0400

    edited card container size and margin

--- script.js
+++ script.js
@@ -133,11 +133,3 @@ function anotherReading() {
 }
 
 setTimeout(anotherReading, 60000)
-
-
-// let text;
-// if (confirm("Press a button!") == true) {
-//     text = "You pressed OK!";
-// } else {
-//     text = "You canceled!";
-// }
